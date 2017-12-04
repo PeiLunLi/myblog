f95bf398c41fc8b9daa3dc55648d7c92ba9d7dac
peiluner <30365887+PeiLunLi@users.noreply.github.com>
2017-12-04 23:42:42 +0800

    Update views.py

--- apps/post/views.py
+++ apps/post/views.py
@@ -7,11 +7,17 @@ from users.models import UserPro
 
 
 def home(request):
-    articles = Article.objects.all()
+    '''
+    主页
+    '''
+    articles=Article.objects.all()
     return render(request, 'post/home.html', {'articles': articles})
 
 
 def show(request):
+    '''
+    文章展示
+    '''
     aid = int(request.GET.get('aid', 0))
     article = Article.objects.get(id=aid)
 
@@ -21,7 +27,10 @@ def show(request):
 
 @login_required
 def edit(request):
-    if request.method == 'POST':
+    '''
+    编辑文章
+    '''
+    if request.method=='POST':
         aid = int(request.POST.get('aid', 0))
         article = Article.objects.get(id=aid)
 
@@ -39,12 +48,18 @@ def edit(request):
 
 
 def search(request):
+    '''
+    文章查询
+    '''
     keyword = request.POST.get('keyword', '')
     result = Article.objects.filter(title__contains=keyword)
     return render(request, 'post/home.html', {'articles': result})
 
 
 def comment(request):
+    '''
+    文章评论功能
+    '''
     aid = int(request.POST.get('aid', 0))
     name = request.POST.get('name', '')
     content = request.POST.get('comment', '')
@@ -53,6 +68,9 @@ def comment(request):
 
 @login_required
 def new_post(request):
+    '''
+    新建文章
+    '''
     if request.method == 'POST':
         uid = int(request.POST.get('uid', 0))
 
