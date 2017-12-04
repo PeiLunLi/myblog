1915dc8ed4d32d9d9df70b8d2c45cca0f578b2ce
peiluner <30365887+PeiLunLi@users.noreply.github.com>
2017-12-04 23:44:02 +0800

    Update adminx.py

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,16 +1,19 @@
 __author__ = 'Pillar.Li'
 import xadmin
 from xadmin import views
+
 from .models import UserPro
+
 
 class BaseSetting(object):
     enable_themes=True
     use_bootswatch=True
 
+    
 class UserProAdmin(object):
-    list_display = ['nick_name', 'birthday', 'gender', 'address','mobile','get_articles_nums']
-    search_fields = ['nick_name', 'birthday', 'gender', 'address','mobile','get_articles_nums']
+    list_display = ['nick_name', 'birthday', 'gender', 'address', 'mobile', 'get_articles_nums']
+    search_fields = ['nick_name', 'birthday', 'gender', 'address', 'mobile', 'get_articles_nums']
 
+    
 # xadmin.site.register(UserPro,UserProAdmin)
-
-xadmin.site.register(views.BaseAdminView,BaseSetting)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
