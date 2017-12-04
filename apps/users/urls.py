e90b7808d4891f9aa6089a144882c1713dae1bcc
peiluner <30365887+PeiLunLi@users.noreply.github.com>
2017-12-04 23:45:22 +0800

    Update urls.py

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+
 from users import views
+
+
 urlpatterns = [
     # url(r'^$', views.home),
     url(r'^login/', views.User_login,name='login'),
-url(r'^logoff/', views.logoff,name='logoff'),
-url(r'^register/', views.register,name='register'),
-
+    url(r'^logoff/', views.logoff,name='logoff'),
+    url(r'^register/', views.register,name='register'),
 ]
