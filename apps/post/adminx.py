11a7c43d5681e54edfc38fbd7add50829913447e
peiluner <30365887+PeiLunLi@users.noreply.github.com>
2017-12-04 23:36:36 +0800

    Update adminx.py

--- apps/post/adminx.py
+++ apps/post/adminx.py
@@ -1,20 +1,18 @@
 import xadmin
-from .models import Article,Comment
 from xadmin import views
 
-
+from .models import Article
+from .models import Comment
 
 class ArticleAdmin(object):
-    list_display =['title','date','content','get_comments_nums']
-    search_fields =['title','date','content','get_comments_nums']
+    list_display=['title', 'date', 'content', 'get_comments_nums']
+    search_fields=['title', 'date', 'content', 'get_comments_nums']
 
 
 class CommentAdmin(object):
-    list_display =['name','date','content']
-    search_fields =['name','date','content']
+    list_display=['name', 'date', 'content']
+    search_fields=['name', 'date', 'content']
 
-
-
-
-xadmin.site.register(Article,ArticleAdmin)
-xadmin.site.register(Comment,CommentAdmin)
+    
+xadmin.site.register(Article, ArticleAdmin)
+xadmin.site.register(Comment, CommentAdmin)
